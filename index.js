7495c83c8bc48d3fcb65e9c4d6ffd3c88a93d6c9
Rguardado1009 <rguardado1009@gmail.com>
2021-08-03 17:49:19 -0400

    db json update and index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-fetch("http://localhost:3000/city")
-	.then((response) => response.json())
-	.then((data) => console.log(data));
+document.addEventListener("DOMContentLoaded", () => {
+	render();
+});
+
+const render = () => {
+	fetch("http://localhost:3000/Location")
+		.then((response) => response.json())
+		.then((weather) => renderData(weather));
+};
+// rendering the data
+function renderData(weatherInfo) {
+	const cityName = document.querySelector(".city");
+	cityName.innerText = weatherInfo["City"];
+
+	const currentTemp = document.querySelector("#Current");
+	currentTemp.innerText = weatherInfo["Current"];
+
+	const skyCondition = document.querySelector(".sky");
+	skyCondition.innerText = weatherInfo["Sky Condition"];
+
+	const highTemp = document.querySelector("#high");
+	highTemp.innerText = weatherInfo["High"];
+
+	const lowTemp = document.querySelector("#low");
+	lowTemp.innerText = weatherInfo["Low"];
+	
+}
